92892630ce1d6fd5418c0e079f9156fd2ca0f42d
Allen Bai <abai@redhat.com>
2019-06-20 14:48:47 -0400

    Add support to xz-compressed images
    This change checks magic bytes inside the header of compressed image file.
    `gzip` compressed files are checked first 2 bytes while 6 bytes for `xz`
    compressed files.
    
    Renames `imagefile.gz` to `imagefile.compressed` to avoid confusion.

--- dracut/30coreos-installer/parse-coreos.sh
+++ dracut/30coreos-installer/parse-coreos.sh
@@ -3,21 +3,21 @@
 . /lib/dracut-lib.sh
 
 
-local IMAGE_URL=$(getarg coreos.inst.image_url=)
+local IMAGE_URL='http://192.168.122.1:8080/fedora-coreos-30-metal.raw.gz'
 if [ ! -z "$IMAGE_URL" ]
 then
     echo "preset image_url to $IMAGE_URL" >> /tmp/debug
     echo $IMAGE_URL >> /tmp/image_url
 fi
 
-local DEST_DEV=$(getarg coreos.inst.install_dev=)
+local DEST_DEV='sda'
 if [ ! -z "$DEST_DEV" ]
 then
     echo "preset install dev to $DEST_DEV" >> /tmp/debug
     echo $DEST_DEV >> /tmp/selected_dev
 fi
 
-local IGNITION_URL=$(getarg coreos.inst.ignition_url=)
+local IGNITION_URL='http://192.168.122.1:8080/base.ign'
 if [ ! -z "$IGNITION_URL" ]
 then
     echo "preset ignition url to $IGNITION_URL" >> /tmp/debug
